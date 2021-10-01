d328e6c805d0a56225f56f112526e3270c65de8e
YuhangXiong <yxiong4@ualberta.ca>
2021-10-01 01:55:52 -0600

    add two models

--- base/views.py
+++ base/views.py
@@ -1,10 +1,24 @@
 from django.shortcuts import render
 from django.http import HttpResponse
+from .models import Room
+
+# rooms = [
+#     {'id': 1, 'name': 'Lets learn Python!'},
+#     {'id': 2, 'name': 'Design with me!'},
+#     {'id': 3, 'name': 'Frontend developers!'}
+# ]
 
 
 def home(request):
-    return render(request, 'home.html')
+    rooms = Room.objects.all()
+    context = {'rooms': rooms}
+    return render(request, 'base/home.html', context)
 
 
-def room(request):
-    return render(request, 'room.html')
+def room(request, pk):
+    room = Room.objects.get(id=pk)
+    # for i in rooms:
+    #     if i['id'] == int(pk):
+    #         room = i
+    context = {'room': room}
+    return render(request, 'base/room.html', context)
